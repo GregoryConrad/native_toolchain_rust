8e94b9d6298b9042a8395c82a7f8f1fb951b951b
Adam Gastineau <adam@agg.im>
2026-08-17 10:52:13 -0700

    fix: resolve rustup from HOME when not in PATH

--- native_toolchain_rust/lib/src/process_runner.dart
+++ native_toolchain_rust/lib/src/process_runner.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:logging/logging.dart';
 import 'package:meta/meta.dart';
 import 'package:native_toolchain_rust/src/exception.dart';
+import 'package:path/path.dart' as path;
 
 @internal
 interface class ProcessRunner {
@@ -35,7 +36,7 @@ interface class ProcessRunner {
       }
       return result.stdout as String;
     } on ProcessException catch (exception, stackTrace) {
-      logger.severe(
+      logger.info(
         'Failed to invoke "$executable $arguments"',
         exception,
         stackTrace,
@@ -52,19 +53,40 @@ extension InvokeRustup on ProcessRunner {
     String? workingDirectory,
     Map<String, String>? environment,
   }) async {
-    try {
-      return await invoke(
-        'rustup',
-        arguments,
-        workingDirectory: workingDirectory,
-        environment: environment,
-      );
-    } on ProcessException catch (e) {
-      throw RustProcessException(
-        'Failed to invoke rustup; is it installed? '
-        'For help installing rust, see https://rustup.rs',
-        inner: e,
-      );
+    late ProcessException lastException;
+    for (final executable in _rustupExecutables) {
+      try {
+        return await invoke(
+          executable,
+          arguments,
+          workingDirectory: workingDirectory,
+          environment: environment,
+        );
+      } on ProcessException catch (e) {
+        lastException = e;
+      }
     }
+
+    throw RustProcessException(
+      'Failed to invoke rustup; is it installed? '
+      'Checked the following paths: $_rustupExecutables. '
+      'For help installing rust, see https://rustup.rs',
+      inner: lastException,
+    );
   }
+}
+
+/// `rustup` executables to attempt to launch, in order of preference
+///
+/// Defaults to `rustup` lookup via PATH, followed by `~/.cargo/bin/rustup`
+List<String> get _rustupExecutables {
+  final homeDirectory = Platform.isWindows
+      ? Platform.environment['USERPROFILE']
+      : Platform.environment['HOME'];
+
+  return [
+    'rustup',
+    if (homeDirectory != null)
+      path.join(homeDirectory, '.cargo', 'bin', 'rustup'),
+  ];
 }
